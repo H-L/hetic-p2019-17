0fb2fb488bb99a51bf35affb776b47ca790d30fc
H-L <hadrien.lepoutre@gmail.com>
2016-10-28 15:19:08 +0200

    [Gulp] Adding gulp-plumber

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,14 @@ var processors = [plugins.autoprefixer];
 
 gulp.task('scss', function () {
 	return gulp.src('scss/main.scss')
+		.pipe(plugins.plumber())
 		.pipe(plugins.sass())
 		.pipe(plugins.postcss(processors))
 		.pipe(gulp.dest('css'))
 });
 
 gulp.task('sync', ['scss'], function () {
-	sync.init({
+	plugins.browserSync.init({
 		server: './'
 	})
 	gulp.watch("scss/**/*.scss", ['scss']);
