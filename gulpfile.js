0d339cc2cceb05050a6fbeebd4d640ffe1b46f90
H-L <hadrien.lepoutre@gmail.com>
2016-10-28 15:55:10 +0200

    [FRONT] Trying to add sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,10 @@ var processors = [plugins.autoprefixer];
 gulp.task('scss', function () {
 	return gulp.src('scss/main.scss')
 		.pipe(plugins.plumber())
+		.pipe(plugins.sourcemaps.init())
 		.pipe(plugins.sass())
 		.pipe(plugins.postcss(processors))
+		.pipe(plugins.sourcemaps.write('.'))
 		.pipe(gulp.dest('css'))
 		.pipe(plugins.notify({
 			message: 'SCSS Compiled'
