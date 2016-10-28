0c8a7fd2a19d5dba920cfb83dc0d990cce594e40
H-L <hadrien.lepoutre@gmail.com>
2016-10-28 15:07:11 +0200

    [FRONT] Setting up Normalize | Setting up gulp scss task

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,20 @@
-var gulp = require('gulp'),
-	sass = require('gulp-sass');
-sync = require('browser-sync')
-	.create();
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var plumber = require('gulp-plumber');
-
-var processors = [
-autoprefixer
-];
-
-gulp.task('scss', function ()
-{
-	return gulp.src('scss/main.scss')
-		.pipe(plumber(
-		{
-			errorHandler: onError
-		}))
-		.pipe(sass())
-		.pipe(postcss(processors))
-		.pipe(gulp.dest('css/main.css'))
-		.pipe(sync.stream());
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')({
+	pattern: '*'
 });
 
-gulp.task('sync', ['scss'], function ()
-{
-	sync.init(
-	{
+var processors = [plugins.autoprefixer];
+
+gulp.task('scss', function () {
+	return gulp.src('scss/main.scss')
+		.pipe(plugins.sass())
+		.pipe(plugins.postcss(processors))
+		.pipe(gulp.dest('css'))
+});
+
+gulp.task('sync', ['scss'], function () {
+	sync.init({
 		server: './'
 	})
 	gulp.watch("scss/**/*.scss", ['scss']);
-
 });
