98d2d6fa7fb8dcec221ac5cd40a8fb2079295396
H-L <hadrien.lepoutre@gmail.com>
2016-11-03 10:42:25 +0100

    [GULP] Adding Minifying plugins for JS and CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,37 @@ var plugins = require('gulp-load-plugins')({
 var processors = [plugins.autoprefixer];
 
 gulp.task('scss', function () {
-	return gulp.src('scss/main.scss')
+	return gulp.src('app/scss/main.scss')
 		.pipe(plugins.plumber())
 		.pipe(plugins.sourcemaps.init())
 		.pipe(plugins.sass())
 		.pipe(plugins.postcss(processors))
+		.pipe(plugins.cleanCss({
+			compatibility: 'ie8'
+		}))
 		.pipe(plugins.sourcemaps.write('.'))
-		.pipe(gulp.dest('css'))
+		.pipe(gulp.dest('dist/css'))
 		.pipe(plugins.notify({
 			message: 'SCSS Compiled'
 		}))
+});
+
+gulp.task('minjs', function () {
+	return gulp.src('app/js/*.js')
+		.pipe(plugins.minify({
+			ext: {
+				src: '-debug.js',
+				min: '.js'
+			},
+			exclude: ['tasks'],
+			ignoreFiles: ['.combo.js', '-min.js']
+		}))
+		.pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('sync', ['scss'], function () {
 	plugins.browserSync.init({
 		server: './'
 	})
-	gulp.watch("scss/**/*.scss", ['scss']);
+	gulp.watch("app/scss/**/*.scss", ['scss']);
 });
